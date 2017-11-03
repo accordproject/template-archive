c4140a8a17fc27e3baa8e2951d4e4f16a31b5ce0
Daniel Selman <danscode@selman.org>
2017-11-03 11:54:53 +0000

    (chore) ci/cd

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 set -e
 date
 echo "Tag is " $1
-lerna publish --yes --conventional-commits -m "chore(release): publish %s"
+lerna publish --yes --canary --conventional-commits -m "chore(release): publish %s"
 echo "Published to npm using lerna."
