205816c4ce4197c687a18c2cea8b966dc7edfdb9
Daniel Selman <danscode@selman.org>
2017-11-03 16:40:49 +0000

    (chore) ci/cd

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,12 @@
-set -e
+# Exit on first error, print all commands.
+set -ev
+set -o pipefail
 date
 echo "Tag is " $1
+
+# Set the NPM access token we will use to publish.
+npm config set registry https://registry.npmjs.org/
+npm config set //registry.npmjs.org/:_authToken ${NPM_TOKEN}
+
 lerna publish --yes --canary --conventional-commits -m "chore(release): publish %s"
 echo "Published to npm using lerna."
