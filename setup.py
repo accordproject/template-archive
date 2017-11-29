04edc86eecfb5957771eca3751a678f8d2bcfedf
Daniel Selman <danscode@selman.org>
2017-11-29 17:28:28 +0000

    setup.py test

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='AccordCiceroDoc',
-    version='0.1dev',
-    packages=['sphinx_js',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-)
