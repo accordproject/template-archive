65502619bbf069d04058d49d89f2218f6a603acf
Daniel Selman <danscode@selman.org>
2017-11-29 17:22:34 +0000

    setup.py test

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from distutils.core import setup
 setup(
     name='AccordCiceroDoc',
     version='0.1dev',
-    packages=['sphinx', 'sphinx_js',],
+    packages=['sphinx_js',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
 )
