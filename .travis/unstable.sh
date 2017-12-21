4d5dd0a9339302684fd9edb694287e9f5ac0175a
Daniel Selman <danscode@selman.org>
2017-12-21 19:06:04 +0000

    (chore) debugging broken npm publish

--- .travis/unstable.sh
+++ .travis/unstable.sh
@@ -2,5 +2,11 @@
 set -ev
 date
 echo "Tag is " $1
+echo "NPM_TOKEN " ${NPM_TOKEN}
+
+# Set the NPM access token we will use to publish.
+npm config set registry https://registry.npmjs.org/
+npm config set //registry.npmjs.org/:_authToken ${NPM_TOKEN}
+
 lerna publish --yes --canary --conventional-commits -m "chore(release): publish %s"
 echo "Published to npm using lerna."
