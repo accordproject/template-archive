4f3066f87bb3195b21a75baa4b15843c8651dc38
Jerome Simeon <simeon@us.ibm.com>
2018-03-05 21:44:34 -0500

    (fix) Consistent use of the same momentjs version in Cicero and Jura

--- packages/cicero-core/lib/tdl.js
+++ packages/cicero-core/lib/tdl.js
@@ -1,7 +1,7 @@
-// Generated automatically by nearley
+// Generated automatically by nearley, version 2.12.1
 // http://github.com/Hardmath123/nearley
 (function () {
-function id(x) {return x[0]; }
+function id(x) { return x[0]; }
 
   const flatten = d => {
     return d.reduce(
