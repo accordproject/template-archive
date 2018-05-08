de8cb08c9853025a1b945a50b03e0de34878d232
Matt Roberts <matt@clause.io>
2018-05-08 15:03:05 +0100

    feat: Stateful API for clause-server
    Signed-off-by: Matt Roberts <matt@clause.io>

--- packages/cicero-server/app.js
+++ packages/cicero-server/app.js
@@ -47,7 +47,15 @@ app.set('port', PORT);
  * The data parameter is either a JSON data file or a TXT file that is used to create
  * the clause from the Template.
  *
+ * Stateless execution
+ * --------------------
  * The HTTP POST body is the request used for execution of the clause.
+ *
+ * Stateful execution
+ * --------------------
+ * If the body contains an object with two properties 'request' and 'state',
+ * then 'request' is used as the execution request,
+ * 'state' is used as the contract state.
  */
 app.post('/execute/:template/:data', async function (req, httpResponse, next) {
     console.log('Template: ' + req.params.template);
@@ -63,7 +71,14 @@ app.post('/execute/:template/:data', async function (req, httpResponse, next) {
             clause.parse(data.toString());
         }
         const engine = new Engine();
-        const result = await engine.execute(clause, req.body);
+        let result;
+        if(Object.keys(req.body).length === 2 &&
+        req.body.hasOwnProperty('request') &&
+        req.body.hasOwnProperty('state')) {
+            result = await engine.execute(clause, req.body.request, req.body.state);
+        } else {
+            result = await engine.execute(clause, req.body);
+        }
         httpResponse.send(result);
     }
     catch(err) {
@@ -71,6 +86,8 @@ app.post('/execute/:template/:data', async function (req, httpResponse, next) {
     }
 });
 
-app.listen(app.get('port'), function () {
+const server = app.listen(app.get('port'), function () {
     console.log('Server listening on port: ', app.get('port'));
 });
+
+module.exports = server;
---
+++ packages/cicero-server/test/data/latedeliveryandpenalty/lib/logic.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/*eslint-disable no-unused-vars*/
+/*eslint-disable no-undef*/
+/*eslint-disable no-var*/
+
+/**
+ * Execute the smart clause
+ * @param {Context} context - the Accord context
+ * @param {org.accordproject.latedeliveryandpenalty.LateDeliveryAndPenaltyRequest} context.request - the incoming request
+ * @param {org.accordproject.latedeliveryandpenalty.LateDeliveryAndPenaltyResponse} context.response - the response
+ * @AccordClauseLogic
+ */
+function execute(context) {
+
+    logger.info(context);
+    var req = context.request;
+    var res = context.response;
+    var contract = context.contract;
+    var now = moment(req.timestamp);
+    var agreed = moment(req.agreedDelivery);
+
+    res.buyerMayTerminate = false;
+    res.penalty = 0;
+
+    if (req.forceMajeure) {
+        logger.info('forceMajeure');
+        res.buyerMayTerminate = true;
+    }
+
+    if (!req.forceMajeure && now.isAfter(agreed)) {
+        logger.info('late');
+        logger.info('penalty duration unit: ' + contract.penaltyDuration.unit);
+        logger.info('penalty duration amount: ' + contract.penaltyDuration.amount);
+        // the delivery is late
+        var diff = now.diff(agreed, contract.penaltyDuration.unit);
+        logger.info('diff:' + diff);
+
+        var penalty = (diff / contract.penaltyDuration.amount) * contract.penaltyPercentage/100 * req.goodsValue;
+
+        // cap the maximum penalty
+        if (penalty > contract.capPercentage/100 * req.goodsValue) {
+            logger.info('capped.');
+            penalty = contract.capPercentage/100 * req.goodsValue;
+        }
+
+        res.penalty = penalty;
+
+        // can we terminate?
+        if (diff > contract.termination.amount) {
+            logger.info('buyerMayTerminate.');
+            res.buyerMayTerminate = true;
+        }
+    }
+}
+
+/*eslint-enable no-unused-vars*/
+/*eslint-enable no-undef*/
