1eed1b509af2bfcab22bab6544cb04ad103392a4
Matt Roberts <code@rbrts.uk>
2018-08-01 10:35:54 +0100

    Add webpack build (#175)
    * Add webpack config
    * Add prepublishonly npm script

---
+++ packages/cicero-core/webpack.config.js
@@ -0,0 +1,71 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+let path = require('path');
+const webpack = require('webpack');
+const packageJson = require('./package.json');
+
+module.exports = {
+    entry: {
+        client: [
+            './index.js'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, 'umd'),
+        filename: 'cicero.js',
+        library: 'cicero',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    plugins: [
+        new webpack.BannerPlugin(`Accord Project, Cicero v${packageJson.version} http://accordproject.org. Copyright 2018, Clause Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: [path.join(__dirname, 'src')],
+                use: ['babel-loader']
+            },
+            {
+                test: /\.ne$/,
+                use:['raw-loader']
+            }
+        ]
+    },
+    node: {
+        fs: 'empty',
+        net: 'empty',
+        tls: 'empty'
+    }
+};
