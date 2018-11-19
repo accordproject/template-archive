020ca5ed58a2107afbbc8337ad4c786a33a6b7fe
Matt Roberts <code@rbrts.uk>
2018-11-19 15:01:01 +0000

    Add transpilation to ECMA 6 for cicero-core
    Signed-off-by: Matt Roberts <code@rbrts.uk>

--- packages/cicero-core/scripts/nunjucks-precompile.js
+++ packages/cicero-core/scripts/nunjucks-precompile.js
@@ -24,5 +24,5 @@ const env = new nunjucks.Environment(new nunjucks.FileSystemLoader('../'), {
     },
     autoescape: false  // Required to allow nearley syntax strings
 });
-const compiled = nunjucks.precompile('lib/template.ne', {env, name: 'template.ne' });
+const compiled = nunjucks.precompile('src/template.ne', {env, name: 'template.ne' });
 fs.writeFileSync(path.resolve(__dirname,'../lib/compiled_template.js'),compiled);
