7e92fddec5615470184a11bc3b0d4119014de6de
Jerome Simeon <jeromesimeon@me.com>
2019-01-04 12:41:25 -0500

    (fix) Ergo tests use overloaded operators
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- packages/cicero-core/test/template.js
+++ packages/cicero-core/test/template.js
@@ -122,7 +122,7 @@ describe('Template', () => {
             template.getDescription().should.equal('Late Delivery and Penalty. In case of delayed delivery except for Force Majeure cases, the Seller shall pay to the Buyer for every 9 DAY of delay penalty amounting to 7% of the total value of the Equipment whose delivery has been delayed. Any fractional part of a DAY is to be considered a full DAY. The total amount of penalty shall not however, exceed 2% of the total value of the Equipment involved in late delivery. If the delay is more than 2 WEEK, the Buyer is entitled to terminate this Contract.');
             template.getVersion().should.equal('0.0.1');
             template.getMetadata().getSample().should.equal('Late Delivery and Penalty. In case of delayed delivery except for Force Majeure cases, the Seller shall pay to the Buyer for every 9 days of delay penalty amounting to 7% of the total value of the Equipment whose delivery has been delayed. Any fractional part of a days is to be considered a full days. The total amount of penalty shall not however, exceed 2% of the total value of the Equipment involved in late delivery. If the delay is more than 2 weeks, the Buyer is entitled to terminate this Contract.');
-            template.getHash().should.equal('556f82b540d8d898ea37e76db66ef420ba27f9065c3f15540d0ba68f8de505eb');
+            template.getHash().should.equal('00847659bcf5b31797d2b71f29a51fe79a3161b9b93893c35ef1962adbb8a125');
             const buffer = await template.toArchive('ergo');
             buffer.should.not.be.null;
             const template2 = await Template.fromArchive(buffer);
@@ -440,7 +440,7 @@ describe('Template', () => {
     describe('#getHash', () => {
         it('should return a SHA-256 hash', async () => {
             const template = await Template.fromDirectory('./test/data/latedeliveryandpenalty');
-            template.getHash().should.equal('556f82b540d8d898ea37e76db66ef420ba27f9065c3f15540d0ba68f8de505eb');
+            template.getHash().should.equal('00847659bcf5b31797d2b71f29a51fe79a3161b9b93893c35ef1962adbb8a125');
         });
     });
 
