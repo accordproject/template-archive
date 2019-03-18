afcbafdead3c58edc1763610eb05c24dfa980f93
Dan Selman <danscode@selman.org>
2019-03-18 22:36:16 +0000

    wip
    Signed-off-by: Dan Selman <danscode@selman.org>

---
+++ packages/cicero-core/src/datetimeformatparser.js
@@ -0,0 +1,102 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const crypto = require('crypto');
+
+/**
+ * Parses a date/time format string
+ * @class
+ * @public
+ * @memberof module:cicero-core
+ */
+class DateTimeFormatParser {
+    /**
+     * Given a format field (like HH or D) this method returns
+     * a logical name for the field.
+     * @param {string} field - the input format field
+     * @returns {string} the field designator
+     */
+    static parseDateTimeFormatField(field) {
+        switch(field) {
+        case 'D':
+        case 'DD':
+            return 'day';
+        case 'M':
+        case 'MM':
+        case 'MMM':
+        case 'MMMM':
+            return 'month';
+        case 'YYYY':
+            return 'year';
+        case 'H':
+        case 'HH':
+            return 'hour';
+        case 'mm':
+            return 'minute';
+        case 'ss':
+            return 'second';
+        case 'SSS':
+            return 'millisecond';
+        case 'TZ':
+            return 'timezone';
+        default:
+            return null;
+        }
+    }
+
+    /**
+     * Converts a format string to a Nearley action
+     * @param {string} formatString - the input format string
+     * @returns {{tokens: String, action: String, name: String }} the tokens and action and name to use for the Nearley rule
+     */
+    static buildDateTimeFormatRule(formatString) {
+        const input = formatString.substr(1,formatString.length -2);
+        const fields = input.split(/(TZ|DD|D|MMMM|MMM|MM|M|YYYY|HH|H|mm|ss|SSS)+/);
+        let tokens = '';
+
+        let parsedDateTime = '{"$class" : "ParsedDateTime",';
+        let fieldNames = [];
+
+        fields.forEach((field, index) => {
+            if(index > 0 && index < fields.length-1) {
+                const fieldName = DateTimeFormatParser.parseDateTimeFormatField(field);
+
+                if(fieldName) {
+                    if(fieldNames.indexOf(fieldName) >= 0) {
+                        throw new Error(`Duplicate ${fieldName} field in date time format string: ${formatString}`);
+                    }
+                    else {
+                        parsedDateTime += `   "${fieldName}": d[${index-1}]`;
+                        tokens += field + ' ';
+                        fieldNames.push(fieldName);
+                    }
+                }
+                else {
+                    tokens += ` "${field}" `;
+                }
+            }
+        });
+
+        parsedDateTime += '}';
+
+        const action = `{% (d) => {return ${parsedDateTime};}%}`;
+        const name = 'DateTime_' + crypto.createHash('md5').update(formatString).digest('hex');
+
+        return { tokens, name, action};
+    }
+}
+
+module.exports = DateTimeFormatParser;
---
+++ packages/cicero-core/test/dateTimeFormatParser.js
@@ -0,0 +1,116 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const DateTimeFormatParser = require('../lib/datetimeformatparser');
+const chai = require('chai');
+
+chai.should();
+chai.use(require('chai-things'));
+chai.use(require('chai-as-promised'));
+
+describe.only('DateTimeFormatParser', () => {
+
+    describe('#parseDateTimeFormatField', () => {
+
+        it('should identity hours', async function() {
+            DateTimeFormatParser.parseDateTimeFormatField('H').should.eql('hour');
+            DateTimeFormatParser.parseDateTimeFormatField('HH').should.eql('hour');
+        });
+        it('should identity days', async function() {
+            DateTimeFormatParser.parseDateTimeFormatField('D').should.eql('day');
+            DateTimeFormatParser.parseDateTimeFormatField('DD').should.eql('day');
+        });
+
+        it('should identity months', async function() {
+            DateTimeFormatParser.parseDateTimeFormatField('M').should.eql('month');
+            DateTimeFormatParser.parseDateTimeFormatField('MM').should.eql('month');
+            DateTimeFormatParser.parseDateTimeFormatField('MMM').should.eql('month');
+            DateTimeFormatParser.parseDateTimeFormatField('MMMM').should.eql('month');
+        });
+
+        it('should identity minutes', async function() {
+            DateTimeFormatParser.parseDateTimeFormatField('mm').should.eql('minute');
+        });
+
+        it('should identity years', async function() {
+            DateTimeFormatParser.parseDateTimeFormatField('YYYY').should.eql('year');
+        });
+
+        it('should identity seconds', async function() {
+            DateTimeFormatParser.parseDateTimeFormatField('ss').should.eql('second');
+        });
+
+        it('should identity milliseconds', async function() {
+            DateTimeFormatParser.parseDateTimeFormatField('SSS').should.eql('millisecond');
+        });
+
+        it('should identity timezone', async function() {
+            DateTimeFormatParser.parseDateTimeFormatField('TZ').should.eql('timezone');
+        });
+
+        it('should return null for anything else', async function() {
+            (DateTimeFormatParser.parseDateTimeFormatField('a') === null).should.be.true;
+        });
+    });
+
+    describe('#buildDateTimeFormatRule', () => {
+
+        it('should parse DD MMM YYYY HH:mm:ss.SSS TZ', async function() {
+            const result = DateTimeFormatParser.buildDateTimeFormatRule('"DD MMM YYYY HH:mm:ss.SSS TZ"');
+            result.tokens.should.eql('DD  " " MMM  " " YYYY  " " HH  ":" mm  ":" ss  "." SSS  " " TZ ');
+            result.action.should.eql('{% (d) => {return {"$class" : "ParsedDateTime",   "day": d[0]   "month": d[2]   "year": d[4]   "hour": d[6]   "minute": d[8]   "second": d[10]   "millisecond": d[12]   "timezone": d[14]};}%}');
+        });
+
+        it('should parse D M YYYY', async function() {
+            const result = DateTimeFormatParser.buildDateTimeFormatRule('"D M YYYY"');
+            result.tokens.should.eql( 'D  " " M  " " YYYY ');
+            result.action.should.eql('{% (d) => {return {"$class" : "ParsedDateTime",   "day": d[0]   "month": d[2]   "year": d[4]};}%}');
+        });
+
+        it('should parse DD/MM/YYYY', async function() {
+            const result = DateTimeFormatParser.buildDateTimeFormatRule('"DD/MM/YYYY"');
+            result.tokens.should.eql( 'DD  "/" MM  "/" YYYY ');
+            result.action.should.eql('{% (d) => {return {"$class" : "ParsedDateTime",   "day": d[0]   "month": d[2]   "year": d[4]};}%}');
+        });
+
+        it('should parse YYYY-MM-DD', async function() {
+            const result = DateTimeFormatParser.buildDateTimeFormatRule('"YYYY-MM-DD"');
+            result.tokens.should.eql( 'YYYY  "-" MM  "-" DD ');
+            result.action.should.eql('{% (d) => {return {"$class" : "ParsedDateTime",   "year": d[0]   "month": d[2]   "day": d[4]};}%}');
+        });
+
+        it('should parse MM/DD/YYYY', async function() {
+            const result = DateTimeFormatParser.buildDateTimeFormatRule('"MM/DD/YYYY"');
+            result.tokens.should.eql( 'MM  "/" DD  "/" YYYY ');
+            result.action.should.eql('{% (d) => {return {"$class" : "ParsedDateTime",   "month": d[0]   "day": d[2]   "year": d[4]};}%}');
+        });
+
+        it('should parse MM/DD/YYYY HH:mm:ss TZ', async function() {
+            const result = DateTimeFormatParser.buildDateTimeFormatRule('"MM/DD/YYYY HH:mm:ss TZ"');
+            result.tokens.should.eql( 'MM  "/" DD  "/" YYYY  " " HH  ":" mm  ":" ss  " " TZ ');
+            result.action.should.eql('{% (d) => {return {"$class" : "ParsedDateTime",   "month": d[0]   "day": d[2]   "year": d[4]   "hour": d[6]   "minute": d[8]   "second": d[10]   "timezone": d[12]};}%}');
+        });
+
+        it('should fail to parse MM/MMM/YYYY', async function() {
+            (()=> DateTimeFormatParser.buildDateTimeFormatRule('"MM/MMM/YYYY"')).should.throw('Duplicate month');
+        });
+
+        it('should fail to parse D/DD/YYYY', async function() {
+            (()=> DateTimeFormatParser.buildDateTimeFormatRule('"D/DD/YYYY"')).should.throw('Duplicate day');
+        });
+
+    });
+});
