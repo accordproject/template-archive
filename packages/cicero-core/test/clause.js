3304f8ba1d7b7de52236ada527cb8bea08e61980
Jerome Simeon <jeromesimeon@me.com>
2019-07-01 16:36:49 -0400

    chore(test) Remove dead code and improve test coverage
    Signed-off-by: Jerome Simeon <jeromesimeon@me.com>

--- packages/cicero-core/test/clause.js
+++ packages/cicero-core/test/clause.js
@@ -277,7 +277,15 @@ describe('Clause', () => {
             const clause = new Clause(template);
             clause.parse(testLatePenaltyInput);
             const nl = clause.generateText();
-            testLatePenaltyInput.should.equal(nl);
+            nl.should.equal(testLatePenaltyInput);
+        });
+
+        it('should be able to generate natural language text with wrapped variables', async function() {
+            const template = await Template.fromDirectory('./test/data/latedeliveryandpenalty', options);
+            const clause = new Clause(template);
+            clause.parse(testLatePenaltyInput);
+            const nl = clause.generateText({ wrapVariables: true });
+            nl.should.equal('Late Delivery and Penalty. In case of delayed delivery{{ except for Force Majeure cases,}} the Seller shall pay to the Buyer for every {{9 days}} of delay penalty amounting to {{7}}% of the total value of the Equipment whose delivery has been delayed. Any fractional part of a {{days}} is to be considered a full {{days}}. The total amount of penalty shall not however, exceed {{2}}% of the total value of the Equipment involved in late delivery. If the delay is more than {{2 weeks}}, the Buyer is entitled to terminate this Contract.');
         });
 
         it('should be able to roundtrip latedelivery natural language text (with a Period)', async function() {
@@ -285,7 +293,7 @@ describe('Clause', () => {
             const clause = new Clause(template);
             clause.parse(testLatePenaltyPeriodInput);
             const nl = clause.generateText();
-            testLatePenaltyPeriodInput.should.equal(nl);
+            nl.should.equal(testLatePenaltyPeriodInput);
         });
 
         it('should be able to roundtrip conga natural language text', async function() {
@@ -293,7 +301,7 @@ describe('Clause', () => {
             const clause = new Clause(template);
             clause.parse(testCongaInput);
             const nl = clause.generateText();
-            testCongaInput.should.equal(nl);
+            nl.should.equal(testCongaInput);
         });
 
         it('should be able to roundtrip alltypes natural language text', async function() {
