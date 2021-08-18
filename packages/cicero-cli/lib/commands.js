f08a5eab556e4a934b12722e61a28b6ec4064aad
sanket shevkar <shevkar.sanket@gmail.com>
2021-08-18 23:02:53 +0530

    refactor(*): signed slc name and contract instance error
    Signed-off-by: sanket shevkar <shevkar.sanket@gmail.com>

--- packages/cicero-cli/lib/commands.js
+++ packages/cicero-cli/lib/commands.js
@@ -674,8 +674,7 @@ class Commands {
                 }
                 else {
                     const instanceName = instance.getIdentifier();
-                    const version = instance.contractSignatures.length;
-                    file = `${instanceName}.v${version}.slc`;
+                    file = `${instanceName}.slc`;
                 }
                 Logger.info('Creating archive: ' + file);
                 fs.writeFileSync(file, archive);
