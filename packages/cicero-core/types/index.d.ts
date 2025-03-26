c9cd1991559a9bd8328c817b786beaeac79f7760
Dan Selman <dselman@meta.com>
2025-03-26 17:59:18 +0000

    types
    Signed-off-by: Dan Selman <dselman@meta.com>

--- packages/cicero-core/types/index.d.ts
+++
@@ -1,5 +0,0 @@
-export const Clause: typeof import("./src/clause");
-export const Template: typeof import("./src/template");
-export const TemplateLoader: typeof import("./src/templateloader");
-export const TemplateLibrary: typeof import("./src/templatelibrary");
-export const version: any;
