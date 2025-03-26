0d931f3a1779e3658e9be87b16115622474f7821
Dan Selman <dselman@meta.com>
2025-03-26 19:41:03 +0000

    tsc
    Signed-off-by: Dan Selman <dselman@meta.com>

--- packages/cicero-core/webpack.config.js
+++ packages/cicero-core/webpack.config.js
@@ -12,26 +12,40 @@
  * limitations under the License.
  */
 
-'use strict';
+"use strict";
 
-let path = require('path');
-const webpack = require('webpack');
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+let path = require("path");
+const webpack = require("webpack");
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
-const packageJson = require('./package.json');
+const packageJson = require("./package.json");
 
 module.exports = {
-    entry: './index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'cicero-core.js',
-        library: {
-            name: 'cicero-core',
-            type: 'umd',
-        },
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "cicero-core.js",
+    library: {
+      name: "cicero-core",
+      type: "umd",
     },
-    plugins: [
-        new webpack.BannerPlugin(`Accord Project Cicero v${packageJson.version}
+  },
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin(`Accord Project Cicero v${packageJson.version}
         Licensed under the Apache License, Version 2.0 (the "License");
         you may not use this file except in compliance with the License.
         You may obtain a copy of the License at
@@ -41,56 +55,36 @@ module.exports = {
         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
         See the License for the specific language governing permissions and
         limitations under the License.`),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.ProvidePlugin({
-            Buffer: ['buffer', 'Buffer'],
-        }),
-        new webpack.ProvidePlugin({
-            process: 'process/browser', // provide a shim for the global `process` variable
-        }),
-        new NodePolyfillPlugin(),
-    ],
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                include: [path.join(__dirname, 'lib')],
-                use: ['babel-loader']
-            },
-            {
-                test: /\.ne$/,
-                use: ['raw-loader']
-            },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ]
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new webpack.ProvidePlugin({
+      process: "process/browser", // provide a shim for the global `process` variable
+    }),
+    new NodePolyfillPlugin(),
+  ],
+  resolve: {
+    fallback: {
+      // Webpack 5 no longer polyfills Node.js core modules automatically.
+      // see https://webpack.js.org/configuration/resolve/#resolvefallback
+      // for the list of Node.js core module polyfills.
+      fs: false,
+      tls: false,
+      net: false,
+      child_process: false,
+      os: false,
+      path: false,
+      // 'crypto': require.resolve('crypto-browserify'),
+      // 'stream': require.resolve('stream-browserify'),
+      // 'http': require.resolve('stream-http'),
+      // 'https': require.resolve('https-browserify'),
+      // 'zlib': require.resolve('browserify-zlib'),
+      // 'vm2': require.resolve('vm-browserify'),
     },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-        fallback: {
-            // Webpack 5 no longer polyfills Node.js core modules automatically.
-            // see https://webpack.js.org/configuration/resolve/#resolvefallback
-            // for the list of Node.js core module polyfills.
-            'fs': false,
-            'tls': false,
-            'net': false,
-            'child_process': false,
-            'os': false,
-            'path': false,
-            // 'crypto': require.resolve('crypto-browserify'),
-            // 'stream': require.resolve('stream-browserify'),
-            // 'http': require.resolve('stream-http'),
-            // 'https': require.resolve('https-browserify'),
-            // 'zlib': require.resolve('browserify-zlib'),
-            // 'vm2': require.resolve('vm-browserify'),
-        }
-    }
+  },
 };
